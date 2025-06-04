3df62b36b6b550c51c5c290d354be93b8271c17a
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-06-03 22:08:49 -0300

    refactor: declaração de records e anotações desnecessárias

--- src/main/java/store/product/ProductOut.java
+++ src/main/java/store/product/ProductOut.java
@@ -2,10 +2,13 @@ package store.product;
 
 import java.io.Serializable;
 import lombok.Builder;
-import lombok.experimental.Accessors;
 
 @Builder
-@Accessors(fluent = true)
-public record ProductOut(String id, String name, Double price, String unit) implements Serializable {
+public record ProductOut(
+    String id,
+    String name,
+    Double price,
+    String unit
+) implements Serializable {
     private static final long serialVersionUID = 1L;
 }
