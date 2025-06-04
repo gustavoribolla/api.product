12ff8416f119e50a0d1ef0588c50211af40e08aa
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-06-03 21:53:54 -0300

    fix: sintaxe de records corrigida

--- src/main/java/store/product/ProductOut.java
+++ src/main/java/store/product/ProductOut.java
@@ -1,17 +1,11 @@
 package store.product;
 
 import java.io.Serializable;
-
 import lombok.Builder;
 import lombok.experimental.Accessors;
 
-@Builder @Accessors(fluent=true)
-public record ProductOut(
-    private static final long serialVersionUID = 1L;
-    String id,
-    String name,
-    Double price,
-    String unit
-) implements Serializable {
+@Builder
+@Accessors(fluent = true)
+public record ProductOut(String id, String name, Double price, String unit) implements Serializable {
     private static final long serialVersionUID = 1L;
 }
